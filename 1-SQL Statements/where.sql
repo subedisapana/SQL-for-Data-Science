67856449e261f6f36db0752ad78672c0086bb570
unknown <sapana.subedi@wiseyak.com>
2023-01-05 23:38:47 +0545

    sql where statement

--- 1-SQL Statements/where.sql
+++ 1-SQL Statements/where.sql
@@ -2,10 +2,23 @@
 tables
 if condn is satisfied, it returns specific value */
 
+/* Syntax: select  <column name> from <table name> where <condition>; */
+
 select * from science_class;
 
 select name from science_class;
 
+/* Equal to condition */
 select name from science_class where science_marks = 33;
 
+/* Less then/ Greater then */
+select name from science_class where Science_Marks > 60;
+
+/* Matching text condition */
 select * from science_class where name = 'Olive';
+
+select distinct name from science_class where name = 'sapana';
+
+select distinct * from science_class where name = 'sapana';
+
+
